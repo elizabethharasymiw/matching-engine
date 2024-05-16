d1677f6df574ba6161fe37a5044032586d7e514f
elizabethharasymiw <elizabeth.harasymiw@gmail.com>
2024-05-15 22:26:35 -0500

    Add functions to work on getting BUY orders to resolve

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,8 @@
 #include <sstream>
 #include <unordered_map>
 #include <vector>
-
+#include <string>
+#include <algorithm>
 
 // Key = Price
 // Value = First Node is total quantity, Following nodes are orderIDs at that price
@@ -19,6 +20,10 @@ std::unordered_map<std::string, int[2]> GFDBuyOrders;
 std::vector<int> sellPriceList;
 std::vector<int> buyPriceList;
 
+bool orderIDInUse(std::string orderID);
+int findThisPriceOrCheaperSellPrice(int price);
+int resolveBuyTrades(int price, int foundPrice, std::string orderID, int quantity);
+void addNewGFDBuyOrder(int price, std::string orderID, int quantity);
 void operationBUY(std::string orderType, std::string orderID, int price, int quantity);
 void operationSELL(std::string orderType, std::string orderID, int price, int quantity);
 void operationCANCEL(std::string orderID);
@@ -78,9 +83,127 @@ int main(int argc, char* argv[]){
     return 0;
 }
 
-void operationBUY(std::string orderType, std::string orderID, int price, int quantity){
-    std::cout << "Found Buy!" << std::endl;
+bool orderIDInUse(std::string orderID){
+    if(GFDSellOrders.find(orderID) != GFDSellOrders.end() || GFDBuyOrders.find(orderID) != GFDBuyOrders.end()){
+        return true; // Found orderID
+    }
+    else {
+        return false; // No orderID found
+    }
+}
+
+int findThisPriceOrCheaperSellPrice(int price){
+
+    if(sellPriceList.empty()){
+        return -1; //none Found
+    }
+
+    sort(sellPriceList.begin(), sellPriceList.end());
+
+    if(sellPriceList[0] <= price){
+        return sellPriceList[0];
+    }
+
+    return -1; //none Found
+}
+
+int resolveBuyTrades(int price, int foundPrice, std::string orderID, int quantity){
+    int possibleTrades = std::min(quantity, std::stoi(GFDActiveSellPrices[foundPrice][0]));
+    int tradesDone = possibleTrades;
+
+    bool noOffersLeftOver = false;
+    if(stoi(GFDActiveSellPrices[foundPrice][0]) == quantity){
+        noOffersLeftOver = true;
+    }
+
+    while(possibleTrades != 0){
+        std::string orderIDToRemove = GFDActiveSellPrices[foundPrice][1];
+        int quantityAvailable = GFDSellOrders[orderIDToRemove][1];
+        int quantityToRemove;
+
+        if(possibleTrades > quantityAvailable){
+            quantityToRemove = quantityAvailable;
+        }
+        else{
+            quantityToRemove = possibleTrades;
+        }
+
+        std::cout << "TRADE " << orderIDToRemove << " " << foundPrice << " " << quantityToRemove
+             << " " << orderID << " " << price << " " << quantityToRemove << std::endl;
+
+        possibleTrades = possibleTrades - quantityToRemove;
+
+        if(possibleTrades == 0){
+            if(GFDSellOrders[orderIDToRemove][1] - quantityToRemove == 0){
+                removeOrderIDFromActiveSellPrices(orderIDToRemove, foundPrice, quantityToRemove);
+                GFDSellOrders.erase(orderIDToRemove);
+            }
+            else{
+                GFDActiveSellPrices[foundPrice][0] = std::to_string(std::stoi(GFDActiveSellPrices[foundPrice][0]) - quantityToRemove);
+                GFDSellOrders[orderIDToRemove][1] = GFDSellOrders[orderIDToRemove][1] - quantityToRemove;
+            }
+        }
+        else{
+             removeOrderIDFromActiveSellPrices(orderIDToRemove, foundPrice, quantityToRemove);
+             GFDSellOrders.erase(orderIDToRemove);
+        }
+    }
+
+    if(noOffersLeftOver || stoi(GFDActiveSellPrices[foundPrice][0]) == 0){
+        GFDActiveSellPrices.erase(foundPrice);
+        removePriceFromSellList(foundPrice);
+    }
+
+    return quantity - tradesDone;
+}
+
+void addNewGFDBuyOrder(int price, std::string orderID, int quantity){
+    if (GFDActiveBuyPrices.find(price) != GFDActiveBuyPrices.end()){
+        // Add new orderID to existing price in GFDActiveBuyPrices
+        int oldQuantity = stoi(GFDActiveBuyPrices[price][0]);
+        GFDActiveBuyPrices[price][0] = std::to_string(oldQuantity + quantity);
+        GFDActiveBuyPrices[price].push_back(orderID);
+    }
+    else{
+        // Add new price to GFDActiveBuyPrices
+        GFDActiveBuyPrices[price][0] = std::to_string(quantity);
+        GFDActiveBuyPrices[price][1] = orderID;
+        buyPriceList.push_back(price);
+    }
+
+    // Update GFDBuyOrders with new order
+    GFDBuyOrders[orderID][0] = price;
+    GFDBuyOrders[orderID][1] = quantity;
+
     return;
+}
+
+void operationBUY(std::string orderType, std::string orderID, int price, int quantity){
+
+   if(price <= 0){
+       return;
+   }
+   if(quantity <= 0){
+       return;
+   }
+   if(orderType != "IOC" && orderType != "GFD"){
+       return;
+   }
+   if(orderIDInUse(orderID)){
+       return;
+   }
+
+    int goodPrice = findThisPriceOrCheaperSellPrice(price);
+    while(goodPrice != -1 && quantity > 0){
+       quantity = resolveBuyTrades(price, goodPrice, orderID, quantity);
+       goodPrice = findThisPriceOrCheaperSellPrice(price);
+    }
+
+    if (quantity > 0 && orderType == "GFD"){
+        addNewGFDBuyOrder(price, orderID, quantity);
+    }
+
+    return
 }
 
 void operationSELL(std::string orderType, std::string orderID, int price, int quantity){
